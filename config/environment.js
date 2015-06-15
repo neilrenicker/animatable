d2ad5569b5c32fc9d313d75efa62949c16a20e2c
Neil Renicker <neilrenicker@gmail.com>
2015-06-15 06:46:34 -0500

    chore: add ember-devtools

--- config/environment.js
+++ config/environment.js
@@ -6,6 +6,10 @@ module.exports = function(environment) {
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
+    'ember-devtools': {
+      global: true,
+      enabled: environment === 'development'
+    },
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
