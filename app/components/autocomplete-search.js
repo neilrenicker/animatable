b2d0c706a3320f8b8a30851b0484a31da07bd5c1
Neil Renicker <neilrenicker@gmail.com>
2015-06-19 07:35:24 -0500

    feat: styled search input and dropdown

--- app/components/autocomplete-search.js
+++ app/components/autocomplete-search.js
@@ -1,11 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  query: null,
+  tagName: 'form',
+  classNames: ['relative'],
+  query: '',
 
   searchResults: Ember.computed('query', 'dataSet', function() {
     var query = this.get('query');
-    if (!query) { return };
 
     var regex = new RegExp(query, 'i');
     return this.get('dataSet').filter(function(result) {
