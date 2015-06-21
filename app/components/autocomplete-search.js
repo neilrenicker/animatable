64d6b67dca0448b96177e88a294bccf1bf97b459
Neil Renicker <neilrenicker@gmail.com>
2015-06-20 22:55:29 -0500

    feat: show / hide select box on focus / unfocus

--- app/components/autocomplete-search.js
+++ app/components/autocomplete-search.js
@@ -4,6 +4,7 @@ export default Ember.Component.extend({
   tagName: 'form',
   classNames: ['relative'],
   query: '',
+  inputHasFocus: false,
 
   searchResults: Ember.computed('query', 'dataSet', function() {
     var query = this.get('query');
@@ -12,5 +13,15 @@ export default Ember.Component.extend({
     return this.get('dataSet').filter(function(result) {
       return result.get('name').match(regex);
     });
-  })
+  }),
+
+  actions: {
+    inputFocused: function() {
+      this.set('inputHasFocus', true)
+    },
+
+    inputUnfocused: function() {
+      this.set('inputHasFocus', false)
+    }
+  }
 });
