c12a3eabc216e75aa92344b5fe7c149ffc66f7d2
Neil Renicker <neilrenicker@gmail.com>
2015-06-23 06:35:24 -0500

    feat: add more animation fixtures

--- app/mirage/fixtures/properties.js
+++ app/mirage/fixtures/properties.js
@@ -26,5 +26,41 @@ export default [
     "name" : "perspective",
     "description" : "The perspective CSS property determines the distance between the z=0 plane and the user in order to give to the 3D-positioned element some perspective. Each 3D element with z>0 becomes larger; each 3D-element with z<0 becomes smaller. The strength of the effect is determined by the value of this property.",
     "content" : "yes, as a length"
+  },
+  {
+    "source" : "/en-US/docs/CSS/color_value#Interpolation",
+    "name" : "color",
+    "sourceUrl" : "https://developer.mozilla.org/en-US/docs/Web/CSS/color",
+    "content" : "yes, as a color",
+    "description" : "The CSS color property sets the foreground color of an element's text content, and its decorations. It doesn't affect any other characteristic of the element; it should really be called text-color and would have been named so, save for historical reasons and its appearance in CSS Level 1."
+  },
+  {
+    "sourceUrl" : "https://developer.mozilla.org/en-US/docs/Web/CSS/opacity",
+    "name" : "opacity",
+    "source" : "/en-US/docs/Web/CSS/opacity",
+    "description" : "The opacity CSS property specifies the transparency of an element, that is, the degree to which the background behind the element is overlaid.",
+    "content" : "yes, as a number"
+  },
+  {
+    "name" : "columns",
+    "source" : "/en-US/docs/Web/CSS/columns",
+    "sourceUrl" : "https://developer.mozilla.org/en-US/docs/Web/CSS/columns",
+    "content" : "as each of the properties of the shorthand: column-width: yes, as a length column-count: yes, as an integer",
+    "description" : "The columns CSS property is a shorthand property allowing to set both the column-width and the column-count properties at the same time."
+  },
+  {
+    "source" : "/en-US/docs/CSS/length#Interpolation",
+    "name" : "column-width",
+    "source" : "/en-US/docs/Web/CSS/column-width",
+    "sourceUrl" : "https://developer.mozilla.org/en-US/docs/Web/CSS/column-width",
+    "content" : "yes, as a length",
+    "description" : "The column-width CSS property suggests an optimal column width. This is not a absolute value but a mere hint. Browser will adjust the width of the column around that suggested value, allowing to achieve scalable designs that fit different screen size. Especially in presence of the column-count CSS property which has precedence, to set an exact column width, all length values must be specified. In horizontal text these are width, column-width, column-gap, and column-rule-width."
+  },
+  {
+    "content" : "yes, as an integer",
+    "description" : "The column-count CSS property describes the number of columns of the element.",
+    "name" : "column-count",
+    "source" : "/en-US/docs/Web/CSS/column-count",
+    "sourceUrl" : "https://developer.mozilla.org/en-US/docs/Web/CSS/column-count",
   }
 ];
