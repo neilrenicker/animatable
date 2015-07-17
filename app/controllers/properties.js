704c3cec8bbb29b6656386509d9e89ce3779d64f
Neil Renicker <neilrenicker@gmail.com>
2015-07-16 22:02:27 -0500

    feat: change body bg-color on submission

--- app/controllers/properties.js
+++ app/controllers/properties.js
@@ -1,6 +1,28 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  defaultCssClass: 'bg-light-silver',
+  positiveResponseCssClass: 'bg-yellow',
+  negativeResponseCssClass: 'bg-teal',
   isPositiveResponse: Ember.computed.match('chosenResult.content', /yes/),
-  isNegativeResponse: Ember.computed.match('chosenResult.content', /no/)
+  isNegativeResponse: Ember.computed.match('chosenResult.content', /no/),
+
+  init: function() {
+    Ember.$('body').addClass(this.get('defaultCssClass'))
+  },
+
+  addBodyColorClass: function() {
+    let negativeClass = this.get('negativeResponseCssClass');
+    let positiveClass = this.get('positiveResponseCssClass');
+    let $body = Ember.$('body')
+
+    // Always remove the default body class if any response is detected
+    $body.removeClass(this.get('defaultCssClass'));
+
+    if (this.get('isPositiveResponse')) {
+      $body.removeClass(negativeClass).addClass(positiveClass)
+    } else if (this.get('isNegativeResponse')) {
+      $body.removeClass(positiveClass).addClass(negativeClass)
+    }
+  }.observes('isPositiveResponse', 'isNegativeResponse')
 });
